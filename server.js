99a5488952f09790e8043c8eab7043b6d69b0d05
haidang91 <haiduc.dang@avenga.com>
2020-11-28 00:54:15 +0100

    google credentials

--- server.js
+++ server.js
@@ -8,15 +8,28 @@ const mongoose = require("mongoose");
 const Image = require("./models/image");
 const nodemailer = require("nodemailer");
 const hbs = require("nodemailer-express-handlebars");
+const {google} = require("googleapis"); 
+const OAuth2 = google.auth.OAuth2; 
+
+const oauth2Client = new OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  "https://developers.google.com/oauthplayground"
+)
+
+oauth2Client.setCredentials({
+  refresh_token: process.env.REFRESH_TOKEN
+});
+const accessToken = oauth2Client.getAccessToken()
 
 const PORT = process.env.PORT || 8080;
 const server = express();
 
-cloudinary.config({
+/* cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
-});
+}); */
 
 /* var corsOptions = {
     origin: '*',
@@ -24,14 +37,14 @@ cloudinary.config({
 }; */
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI, (error) => {
+/* mongoose.connect(process.env.MONGODB_URI, (error) => {
   if (error) {
     console.error("Please make sure Mongodb is installed and running!"); // eslint-disable-line no-console
     throw error;
   }
-});
+}); */
 
-mongoose.set("debug", true);
+/* mongoose.set("debug", true); */
 
 server.use(formData.parse());
 
@@ -73,8 +86,7 @@ server.post("/image-upload", (req, res) => {
 });
 
 // Download path
-server.get("/freebies/:email", (req, res) => {
-  /* res.download('./assets/gifs.rar', 'freebieGifs.rar', function(err) {
+/* res.download('./assets/gifs.rar', 'freebieGifs.rar', function(err) {
         if(err){
             console.log(err)
         } else {
@@ -82,12 +94,19 @@ server.get("/freebies/:email", (req, res) => {
         }
     }) */
 
+server.get("/freebies/:email", (req, res) => {
+  
+
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "thecottagedreamers@gmail.com",
-      pass: process.env.EMAIL_PASSWORD, // naturally, replace both with your real credentials or an application-specific password
-    },
+      type: "OAuth2",
+      user: "thecottagedreamers@gmail.com", 
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
+      accessToken: accessToken
+ }
   });
 
   const options = {
