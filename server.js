7ffb1f6f2fb7354869d283df2c87c1158d383095
haidedang <haiduc.dang91@gmail.com>
2020-11-29 15:29:07 +0100

    update email and google auth client

--- server.js
+++ server.js
@@ -9,31 +9,28 @@ const Image = require("./models/image");
 const User = require("./models/user");
 const nodemailer = require("nodemailer");
 const hbs = require("nodemailer-express-handlebars");
-const { google } = require("googleapis");
-
-
-const OAuth2 = google.auth.OAuth2;
+const {google} = require("googleapis"); 
+const OAuth2 = google.auth.OAuth2; 
 
 const oauth2Client = new OAuth2(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET,
   "https://developers.google.com/oauthplayground"
-);
+)
 
 oauth2Client.setCredentials({
-  refresh_token: process.env.REFRESH_TOKEN,
+  refresh_token: process.env.REFRESH_TOKEN
 });
-
-const accessToken = oauth2Client.getAccessToken();
+const accessToken = oauth2Client.getAccessToken()
 
 const PORT = process.env.PORT || 8080;
 const server = express();
 
-/* cloudinary.config({
+cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
-}); */
+});
 
 /* var corsOptions = {
     origin: '*',
@@ -41,26 +38,21 @@ const server = express();
 }; */
 
 // MongoDB Connection
-/* mongoose.connect(process.env.MONGODB_URI, (error) => {
+mongoose.connect(process.env.MONGODB_URI, (error) => {
   if (error) {
     console.error("Please make sure Mongodb is installed and running!"); // eslint-disable-line no-console
     throw error;
   }
 });
 
-mongoose.set("debug", true); */
+mongoose.set("debug", true);
 
 server.use(formData.parse());
 
-//server.use(cors(corsOptions));
-
-/* server.use(cors());
- */
-
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
+  res.header('Access-Control-Allow-Origin', '*'); 
+  next()
+})
 
 server.get("/", function (req, res) {
   res.send("Welcome at the cottage dream");
@@ -89,7 +81,7 @@ server.post("/image-upload", (req, res) => {
     .catch((err) => res.status(400).json(err));
 });
 
-/* server.get("/freebies", (req, res) => {
+server.get("/freebies", (req, res) => {
   res.download('./assets/freebieGifs.zip', 'freebieGifs.zip', function(err) {
     if(err){
         console.log(err)    
@@ -97,11 +89,10 @@ server.post("/image-upload", (req, res) => {
         console.log('File sent')
     }
 })
-}) */
+})
 
-// Save email to DB
 server.get("/:email", (req, res) => {
-/*   const newUser = new User();
+    const newUser = new User();
   newUser.email = req.params.email;
 
   newUser.save((err, saved) => {
@@ -109,38 +100,43 @@ server.get("/:email", (req, res) => {
       console.log(err);
     }
     res.json({ status: "OK" });
-  }); */
+  });
+  
 
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
       type: "OAuth2",
-      user: "thecottagedreamers@gmail.com", //your gmail account you used to set the project up in google cloud console"
+      user: "thecottagedreamers@gmail.com", 
       clientId: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
       refreshToken: process.env.REFRESH_TOKEN,
-      accessToken: accessToken //access token variable we defined earlier
-    },
+      accessToken: accessToken
+ }
   });
 
   const options = {
     viewEngine: {
       partialsDir: __dirname + "/views/partials",
       layoutsDir: __dirname + "/views/layouts",
-      extname: ".hbs",
+      extname: ".hbs"
     },
     extName: ".hbs",
-    viewPath: "views",
+    viewPath: "views"
   };
+  
 
-  transporter.use("compile", hbs(options));
+  transporter.use(
+    "compile",
+    hbs(options)
+  );
 
   const mailOptions = {
     from: "thecottagedreamers@gmail.com",
     to: req.params.email,
     subject: "Here is your Gift from Us :)",
     template: "index",
-    attachments: [{ path: "./assets/freebieGifs.zip" }],
+    attachments:[{path:"./assets/freebieGifs.zip"}]
   };
 
   transporter.sendMail(mailOptions, function (error, info) {
@@ -148,7 +144,7 @@ server.get("/:email", (req, res) => {
       console.log(error);
     } else {
       console.log("Email sent:" + info.response);
-      res.send("OK");
+      res.send('OK')
     }
   });
 });
